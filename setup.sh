bc075c3445d089d219d198eedac5c9573ae1aa02
takanamito <takanamito0928@gmail.com>
2015-12-31 22:04:23 +0900

    add zsh install and config

--- setup.sh
+++ setup.sh
@@ -1,3 +1,11 @@
+# zshのインストール
+chsh -s /usr/local/bin/zsh
+
+# oh-my-zshのインストール
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+# plugins=(git ruby osx bundler brew rails emoji-clock)
+# ZSH_THEME="bureau"
+
 read -p "install dotfiles? y/n " yn
 if [ $yn = "y" -o $yn = "Y" ]; then
     DOT_FILES=(.bashrc .bash_profile .gitignore_global .gitconfig .gitignore .gvimrc .tmux.conf .vimrc .vim)
